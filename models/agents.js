e58083ab2b93e121a0be4f9def9bf04a75d519ec
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2020-12-30 22:52:42 +0100

    changed rewards in agentSchema to Points

--- models/agents.js
+++ models/agents.js
@@ -8,7 +8,7 @@ var agentSchema = new mongoose.Schema({
     createdBy:{type: String, default: "Guest"}, 
     wins: {type: Number, default:0}, 
     losses: {type: Number, default: 0},
-    rewards: {type: Number, default: 0},  
+    points: {type: Number, default: 0},  
     createdOn: {type: Date, default: Date.now}, 
     states:[states]
 });
