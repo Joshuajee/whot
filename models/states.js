d8a624f8fed55805cb6907dbcb1b7725b34139f7
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-06 12:17:07 +0100

    update rules in state schema to include active and defend

--- models/states.js
+++ models/states.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 
 //schema for state as a subdocument of agent
 var stateSchema = new mongoose.Schema({ 
+
     cardAtHand:{type:Array, default:[]},
     cardInPlay:{type:String, default:""},
     cardPlayed:{type:Array, default:[]},
@@ -17,7 +18,8 @@ var stateSchema = new mongoose.Schema({
                                     "generalMarket":{"active":true, "card":14, "defend":false}
                                 }, 
     createdOn: {type: Date, default: Date.now}, 
-    lastUpdatedOn: {type: Date, default: Date.now}, 
-});
+    lastUpdatedOn: {type: Date, default: Date.now}
+    }
+})
 
 module.exports =  stateSchema
