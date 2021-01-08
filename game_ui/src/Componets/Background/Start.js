c4021d67226d5efaa3b5ee78b841d22618af3fd5
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-08 22:04:25 +0100

    finished designing start page

--- game_ui/src/Componets/Background/Start.js
+++ game_ui/src/Componets/Background/Start.js
@@ -5,20 +5,80 @@ import TriangleCard from "../Cards/TriangleCard";
 import StarCard from "../Cards/StarCard";
 import WhotCard from "../Cards/WhotCard";
 import CardCover from "../Cards/CardCover";
+import Cards from "../../GameEnv/cards";
 
 function Start(props) {
 
+    let cards =  shuffle(Cards.cards)
+    let componet = []
+
+
+    for (let i = 0; i < cards.length; i++) {
+        
+        let index = cards[i].indexOf(":")
+        let number = parseInt(cards[i].slice(index + 1, cards[i].length))
+        let shape = cards[i].slice(0, index)
+
+        console.log(shape)
+
+        switch(shape){
+
+            case "star":
+                componet.push(<StarCard size={100} number={number} />)
+            break
+
+            case "circle":
+                componet.push(<CircleCard size={100} number={number} />)
+            break
+
+            case "square":
+                componet.push(<SquareCard size={100} number={number} />)
+            break
+
+            case "cross":
+                componet.push(<CrossCard size={100} number={number} />)
+            break
+
+            case "triangle":
+                componet.push(<TriangleCard size={100} number={number} />)
+            break
+
+            case "whot":
+                componet.push(<WhotCard size={100} number={number} />)
+            break
+            default:
+                componet.push(<CardCover size={100}  />)
+
+        }
+        
+    }
+   
+
     return(
         <div className="start-page" >
-            <SquareCard size={100} number={1} />
-            <CircleCard size={100} number={2} />
-            <CrossCard size={100} number={5} />
-            <TriangleCard size={100} number={14} />
-            <StarCard size={100} number={8} />
-            <WhotCard size={100} number={20} />
-            <CardCover size={100}  />
+            {componet}
         </div>
         )
+}
+
+function shuffle(array){
+
+    let currIndex = array.length
+
+    while (0 !== currIndex) {
+        
+        //pick an element not prevously selected
+        let randIndex = Math.floor(Math.random() * currIndex)
+        currIndex--
+
+        //swap it with the current element
+        let tempVal  = array[currIndex]
+        array[currIndex] = array[randIndex]
+        array[randIndex] = tempVal
+    
+    }
+
+    return array
 }
 
 export default Start
--- game_ui/src/index.js
+++ game_ui/src/index.js
@@ -3,54 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import  "./Styles/styles.css";
 import reportWebVitals from './reportWebVitals';
-import SquareCard from "./Componets/Cards/SquareCard";
-import CircleCard from "./Componets/Cards/CircleCard";
-import CrossCard from "./Componets/Cards/CrossCard";
-import TriangleCard from "./Componets/Cards/TriangleCard";
-import StarCard from "./Componets/Cards/StarCard";
-import WhotCard from "./Componets/Cards/WhotCard";
-import CardCover from "./Componets/Cards/CardCover";
 import Start from "./Componets/Background/Start";
 import Button from "./Componets/Button";
 
-///<Start 
-//width={window.screen.width}
-//height={window.screen.height}
-///>
-
-/*
-      <SquareCard size={100} number={1} />
-        <CircleCard size={100} number={2} />
-        <CrossCard size={100} number={5} />
-        <TriangleCard size={100} number={14} />
-        <StarCard size={100} number={8} />
-        <WhotCard size={100} number={20} />
-        <CardCover size={100}  />
-*/
 ReactDOM.render(
   <React.StrictMode>
- 
-
-
   
-  
-  <center>
+    <center>
 
-    <Start />
+      <Start />
 
-    <div className="start-button">
+      <div className="start-button">
 
-      <Button text={"Start Game"} class={"btn-start"}/>
+        <Button text={"Start Game"} class={"btn-start"}/>
 
-      <Button text={"Options"} class={"btn-options"}/>
+        <Button text={"Options"} class={"btn-options"}/>
 
-      <Button text={"Settings"} class={"btn-settings"}/>
-      
-    </div>
-  
-  </center>
+        <Button text={"Settings"} class={"btn-settings"}/>
         
- 
+      </div>
+    
+    </center>
+        
   </React.StrictMode>,
   document.getElementById('root')
 );
