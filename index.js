7ed723dffafa384ed5722f360914b951ac27e830
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-12 17:26:36 +0100

    server now serve the front-end static content

--- index.js
+++ index.js
@@ -42,6 +42,23 @@ app.post('/add_agents', (req, res) =>{
 })
 
 
+//check if we are in a production environment
+if(process.env.NODE_ENV === "production"){
+    
+    //serve static production asset
+    app.use(express.static("game_ui/build"))
+
+    //get the current file path
+    const path = require('path')
+
+    //handle any route that is missing from the Server
+    app.get('*', (req, res) =>{  
+        res.sendFile(path.resolve(__dirname, "game_ui", "build", "index.html"))
+        console.log(path.resolve(__dirname, "game_ui", "build", "index.html"))
+    })
+
+}
+
 
 const PORT = process.env.PORT || 10000
 
