841518e560fe3b0a49ca33f876d5e29d92ffb8a2
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-16 21:30:57 +0100

    fixed table height

--- game_ui/src/Routes/GamePlay.js
+++ game_ui/src/Routes/GamePlay.js
@@ -3,9 +3,13 @@ import Market from "../Componets/CardHolder/Market";
 import Player from "../Componets/CardHolder/Player";
 
 function GamePlay() {
+    
+    let height = window.innerHeight
+
+    const style = {height: height * 0.9}
 
     return(<div>
-            <center id="game-table" className="game-table">
+            <center id="game-table" style={style} className="game-table">
                 <Player top={0.2} angle={180} cards={["triangle:2", "star:2", "whot:20"]} />
                 <Player top={0.8} angle={0} cards={["square:14", "cross:5", "circle:10"]} />
                 <InPlay className="center" cards="star:8" />
