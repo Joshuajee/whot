3889303d9afab8b591574a0e1cd94c32a8df4d68
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-17 23:20:16 +0100

    made GamePlay a class based componet

--- game_ui/src/Routes/GamePlay.js
+++ game_ui/src/Routes/GamePlay.js
@@ -1,22 +1,62 @@
+import React from "react"
 import InPlay  from "../Componets/CardHolder/InPlay";
 import Market from "../Componets/CardHolder/Market";
 import Player from "../Componets/CardHolder/Player";
+import Cards from "../GameEnv/cards"
+import {shuffle, goMarket} from "../GameLogic/logics";
 
-function GamePlay() {
+const card = [...Cards.cards]
+
+class GamePlay extends React.Component{
+
+
+    constructor(){
+
+        super()
+ 
+        this.market = [...shuffle(card)]
+
+        this.player = []
+        this.opponent = []
+        this.cardPlayed = []
+        this.rules = []
+
+        goMarket(this.player, this.market, 3)
+        goMarket(this.opponent, this.market, 3)
+        goMarket(this.cardPlayed, this.market, 1)
+
+
+        //for Agent
+
+        this.state = {
+            cardPlayed : this.cardPlayed,
+            cardInPlay : null,
+            noOfCardInMarket : this.market.length,
+            noOfCardsWithOpponet: this.player.length,
+            cardAtHand: this.opponent,
+            availableMove:null,
+            rules:{}
+        }
+
+        console.log(this.state)
+    }
+
+    render(){
     
-    let height = window.innerHeight
+        let height = window.innerHeight
 
-    const style = {height: height * 0.9}
+        const style = {height: height * 0.9}
 
-    return(<div>
-            <center id="game-table" style={style} className="game-table">
-                <Player top={0.2} angle={180} cards={["triangle:2", "star:2", "whot:20"]} />
-                <Player top={0.8} angle={0} cards={["square:14", "cross:5", "circle:10"]} />
-                <InPlay className="center" cards="star:8" />
-                <Market />
-            </center>
-        </div>
-    )
+        return(<div>
+                <center id="game-table" style={style} className="game-table">
+                    <Player top={0.2} angle={180} cards={this.opponent} postion={this.state} />
+                    <Player top={0.8} angle={0} cards={this.player} postion={this.state} />
+                    <InPlay className="center" cards="star:8" />
+                    <Market />
+                </center>
+            </div>
+        )
+    }
 
 }
 
