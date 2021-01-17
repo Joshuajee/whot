0a238704f1a2583c6b1d5cf05ad3921fa35d83f2
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-17 23:05:28 +0100

    fixed export error

--- game_ui/src/GameLogic/logics.js
+++ game_ui/src/GameLogic/logics.js
@@ -1,5 +1,5 @@
 
-function shuffle(array){
+export function shuffle(array){
 
     let currIndex = array.length
 
@@ -19,4 +19,25 @@ function shuffle(array){
     return array
 }
 
-export default shuffle
+export function goMarket(player,  market, times = 1){
+
+    //+---------------------------------------------------------------------------+
+    //|      This method adds card to player and remove that same card from       |  
+    //|      market for n number of times                                         |
+    //+---------------------------------------------------------------------------+
+
+    for(let i = 0; i < times; i ++){
+
+        if(market.length > 0){
+
+            player.push(market[market.length - 1])
+
+            market.pop()
+
+        }
+
+    }
+
+}
+
+//export {shuffle, goMarket}
