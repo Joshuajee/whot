f87b1022f9c99c7bf54c2f0eea2a2821a3db1934
Joshuajee <36106199+Joshuajee@users.noreply.github.com>
2021-01-18 15:04:54 +0100

    fixed leaderboard routes

--- game_ui/src/Routes/Leaderboard.js
+++ game_ui/src/Routes/Leaderboard.js
@@ -1,22 +1,57 @@
+import React from "react"
 import Start from "../Componets/Background/Start";
 import Agent from "../Componets/Agent";
+import axios from "axios"
+
+class Leaderboard extends React.Component {
+
+    constructor(){
+
+        super()
+
+        this.state = {
+            isLoading:true,
+            response:{}
+        }
 
 
-function Leaderboard() {
+    }
 
-    return(
-        <center>
+    componentDidMount(){
+        axios.get("/api/leaderboard/0").then((response) =>{
+            console.log(response)
+            this.setState({
+                isLoading:false,
+                response:response.data
+            })
+        })
+    }
+    
+    render(){
+        if(this.state.isLoading) return (<div>Loading</div>)
+        let data = this.state.response
+        let agents = []
+        for (let i = 0; i < data.length; i++) {
+            agents.push(<Agent agentName={data[i].agentName} 
+                            points={data[i].points} 
+                            wins={data[i].wins}
+                            losses={data[i].losses} />)
+        }
 
-            <Start />
+        return(
+            <div>
 
-            <div className="start-button">
+                <Start />
 
-                <Agent />
+                <div className="leaderboard">
 
+                    {agents}
+
+                </div>
+                
             </div>
-            
-        </center>
-    )
+        )
+    }
 
 }
 
